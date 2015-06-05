681de320bc56c300bc60746a0c03c8d79762cf59
Sergey Lvov <s.lvov@securitycode.ru>
2015-06-05 16:49:42 +0300

    __PRETTY_FUNCTION__

--- include/Logging.h
+++ include/Logging.h
@@ -3,4 +3,8 @@
 
 #include <boost/log/trivial.hpp>
 
-#define BOOST_LOG_TRACE BOOST_LOG_TRIVIAL(trace) << "[" << __FILE__ << "::" << __LINE__ << "::" << __FUNCTION__ << "] "
+#ifdef _MSC_VER
+#define __PRETTY_FUNCTION__ __FUNCDNAME__
+#endif
+
+#define BOOST_LOG_TRACE BOOST_LOG_TRIVIAL(trace) << __PRETTY_FUNCTION__ << ":" << __LINE__ << ": "
